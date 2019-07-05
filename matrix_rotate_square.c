58450e57fd86a90f5538d5e6610d039d50536511
Marius <marius.feteanu@gmail.com>
2019-07-05 23:54:19 +0100

    add some comments explaing how this matrix rotation works

--- matrix_rotate_square.c
+++ matrix_rotate_square.c
@@ -1,32 +1,42 @@
 #include "matrix.h"
 
 void rotate(matrix img){
-    int n=img.nrows-1;
+    int n = img.nrows-1; // maximum index
 
-    int sc = 0;
-    int ec = img.nrows-2;
-    int i = 0;
+    int sc = 0; // start column for element rotation
+    int ec = n; // end column for element rotation (exclusive of)
+    int i = 0;  // current row
 
+    // We rotate in place (see below)
+    // at positions marker as "o".
+    // Positions marked as "x" are filled in
+    // by the rotations, and as such we don't need
+    // to start rotating from them.
+    // o o o x
+    // x o x x
+    // x x x x
+    // x x x x
     while(sc <= ec){
-        for(int j=sc; j<=ec; j++){
-            printf("%d %d\n", i, j);
+        for(int j=sc; j<ec; j++){ // rotate all needed cols in current row
+            //  we rotate in place four corners
+            //  starting at a "starting" position (i, j)
+            //  0  ==> 1
+            //  /\     ||
+            //  ||     \/
+            //  3  <== 2
+            int p0 = mx_get(img, i, j);
+            int p1 = mx_get(img, j, n-i);
+            int p2 = mx_get(img, n-i, n-j);
+            int p3 = mx_get(img, n-j, i);
 
-            int p1, p2;
-
-            p1 = mx_get(img, j, n-i);
-            mx_set(img, j, n-i, mx_get(img, i, j));
-
-            p2 = mx_get(img, n-i, n-j);
+            mx_set(img, j, n-i, p0);
             mx_set(img, n-i, n-j, p1);
-
-            p1 = mx_get(img, n-j, i);
             mx_set(img, n-j, i, p2);
-
-            mx_set(img, i, j, p1);
+            mx_set(img, i, j, p3);
         }
-        sc++;
-        ec--;
-        i++;
+        sc++; // skip one col left after rotating all columns in current row
+        ec--; // skip one col right after rotating all columns in current row
+        i++;  // go to the next row
     }
 }
 
