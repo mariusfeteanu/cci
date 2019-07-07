5bfd27db4bd8e63bbff374cc3680be3da512bdc6
Marius <marius.feteanu@gmail.com>
2019-07-07 14:16:50 +0100

    function to create empty list

--- common/lili.c
+++ common/lili.c
@@ -4,6 +4,11 @@
 #include "lili.h"
 
 // manipulation
+ll_list *ll_empty()
+{
+    return calloc(1, sizeof(ll_node));
+}
+
 void ll_insert(ll_list * list, void *key, size_t key_size)
 {
     ll_node *node = malloc(sizeof(ll_node));
@@ -30,10 +35,12 @@ void ll_remove(ll_list * list, ll_node * node)
 }
 
 // Equality tests
-int ll_node_eq_value(ll_node *node, char *value, int size){
+int ll_node_eq_value(ll_node * node, char *value, int size)
+{
     char *node_value = (char *) node->key;
 
-    if(!node) return 0;
+    if (!node)
+        return 0;
 
     for (int i = 0; i < size; i++) {
         if (node_value[i] != value[i]) {
@@ -43,7 +50,8 @@ int ll_node_eq_value(ll_node *node, char *value, int size){
     return 1;
 }
 
-int ll_node_eq(ll_node *left, ll_node *right){
+int ll_node_eq(ll_node * left, ll_node * right)
+{
     if (left == right)
         return 1;
     if (left->key_size != right->key_size)
@@ -67,7 +75,8 @@ int ll_eq(ll_list left_list, ll_list right_list)
     ll_node *right = right_list.head;
 
     while (left && right) {
-        if(!ll_node_eq(left, right)) return 0;
+        if (!ll_node_eq(left, right))
+            return 0;
         left = left->next;
         right = right->next;
     }
@@ -80,7 +89,7 @@ ll_node *ll_search(ll_list list, void *value, int size)
     ll_node *node = list.head;
 
     while (node) {
-        if(ll_node_eq_value(node, (char *)value, size))
+        if (ll_node_eq_value(node, (char *) value, size))
             return node;
         node = node->next;
     }
