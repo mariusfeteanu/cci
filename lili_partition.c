93009a385a519731043dfc61dd8e0bdf57d5497a
Marius <marius.feteanu@gmail.com>
2019-07-10 07:43:31 +0100

    fix ll partitioning

--- lili_partition.c
+++ lili_partition.c
@@ -12,30 +12,28 @@ void ll_partition(ll_list * list, int pv)
 
         if (nv >= pv) {
             // cut from current list
-           if(node->prev){
-               node->prev->next = node->next;
-           } else {
-             list->head = node->next;
-             p2->head = node;
-             p2->tail = node;
-           }
-           if(node->next){
-               node->next->prev = node->prev;
-           } else {
-             list->tail = node->prev;
-           }
-           // paste to other list
-           node->prev = p2->tail;
-           node->next = NULL;
-           if(p2->tail){
-             p2->tail->next = node;
-           }
+            if (node->prev) {
+                node->prev->next = node->next;
+            } else {
+                list->head = node->next;
+            }
+            if (node->next) {
+                node->next->prev = node->prev;
+            } else {
+                list->tail = node->prev;
+            }
 
-           // free(node);
-            // paste to new list
-            // This is simpler but slower
-            // ll_insert(p2, node->key, node->key_size);
-            // ll_remove(list, node);
+            // paste to other list
+            node->prev = p2->tail;
+            node->next = NULL;
+
+            if (!p2->head) {
+                p2->head = node;
+            }
+            if (p2->tail) {
+                p2->tail->next = node;
+            }
+            p2->tail = node;
         }
         node = next;
     }
