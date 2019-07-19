f0bd4ac33bfba6321a4eb1b1de203ba4626dab80
Marius <marius.feteanu@gmail.com>
2019-07-19 19:26:42 +0100

    fix memory leaks

--- lili_intersect.c
+++ lili_intersect.c
@@ -12,6 +12,9 @@ ll_node *ll_intersection(ll_node * left, ll_node * right)
         ll_node *longer = right;
         right = left;
         left = longer;
+        int long_length = right_length;
+        right_length = left_length;
+        left_length = long_length;
     }
 
     int diff = left_length - right_length;
@@ -37,7 +40,8 @@ ll_node *ll_intersection(ll_node * left, ll_node * right)
 
 #ifdef TEST_lili_intersect
 void test_ll_is_intersection(ll_node * left,
-                             ll_node * right, ll_node * intersect)
+                             ll_node * right,
+                             ll_node * intersect)
 {
     printf("Checking that the following lists intersect:\n");
     ll_print(left);
@@ -46,6 +50,20 @@ void test_ll_is_intersection(ll_node * left,
     assert(ll_intersection(left, right) == intersect);
 
     printf("Looks good!\n");
+
+    ll_node *next;
+
+    while(left != intersect){
+        next = left->next;
+        free(left);
+        left = next;
+    }
+    while(right != intersect){
+        next = right->next;
+        free(right);
+        right = next;
+    }
+    ll_destroy(intersect);
 }
 
 void test_ll_is_not_intersection(ll_node * left, ll_node * right)
@@ -57,6 +75,9 @@ void test_ll_is_not_intersection(ll_node * left, ll_node * right)
     assert(ll_intersection(left, right) == NULL);
 
     printf("Looks good!\n");
+
+    ll_destroy(left);
+    ll_destroy(right);
 }
 
 int main()
@@ -66,6 +87,13 @@ int main()
     ll_node *left = ll_from_array((int[]) { 1, 2, 1, 4 }, 4);
     ll_node *right = left->next->next;
     right = ll_insert(right, 8);
+    test_ll_is_intersection(left, right, left->next->next);
+
+    left = ll_from_array((int[]) { 1, 2, 1, 4 }, 4);
+    right = left->next->next;
+    right = ll_insert(right, 8);
+    right = ll_insert(right, 9);
+    right = ll_insert(right, 7);
     test_ll_is_intersection(left, right, left->next->next);
 
     left = ll_from_array((int[]) { 1, 2, 1, 4 }, 4);
